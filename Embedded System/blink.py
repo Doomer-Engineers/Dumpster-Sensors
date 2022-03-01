a39750dbb8e8660c8106afe4787b01ba08d0a7de
henry-bulluck <thbulluck@gmail.com>
2022-02-28 18:25:20 -0600

    fixed blink file

--- Embedded System/blink.py
+++ Embedded System/blink.py
@@ -4,7 +4,8 @@ from machine import Pin
 led=Pin(22,Pin.OUT)
 
 while True:
-led.value(1)
-time.sleep(1)
-led.value(0)
-time.sleep(1)
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
+
