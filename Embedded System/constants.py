1c98ac02480512d7dea657a8fba258bede06b53e
henry-bulluck <thbulluck@gmail.com>
2022-04-03 19:12:08 -0500

    updated main

--- Embedded System/constants.py
+++ Embedded System/constants.py
@@ -5,3 +5,8 @@ HOST_NAME = "sheltered-citadel-31729.herokuapp.com"
 PATH_NAME = "/database_connection.php"
 
 SENSOR_ID = 1
+
+MAX_DEPTH_CM = 300
+NEAR_FULL_PERCENT = 80
+
+DEEPSLEEP_TIME_MIN = 1
--- Embedded System/main.py
+++ Embedded System/main.py
@@ -1,13 +1,47 @@
+import constants
+from machine import Pin
+from machine import deepsleep
 from sensor import Sensor
 import database_connection as dbc
+
+relay_pin = led=Pin(??,Pin.OUT)
+relay_pin.value(0)
+
+maintenance_mode_pin = machine.Pin(??, machine.Pin.IN)
 
 sensor_1 = Sensor(trigger_pin=17, echo_pin=16, echo_timeout_us=115200)
 sensor_2 = Sensor(trigger_pin=15, echo_pin=14, echo_timeout_us=115200)
 
 def run():
-    dist_1 = sensor_1.distance_cm(self)
-    dist_2 = sensor_2.distance_cm(self)
+    # If the maintenance pin is down
+    if (maintenance_mode_pin.value() == 0):
+        # Turns sensors on for input
+        relay_pin.value(1)
+        # Reads in distance in cm from each
+        dist_1 = sensor_1.distance_cm(self)
+        dist_2 = sensor_2.distance_cm(self)
+        # If an negative value is given, the sensors are disconnected
+        if (dist_1 < 0 or dist_2 < 0):
+            dbc.alertQuery("Disconnected sensor")
+        else:
+            # Calculates what percentage of each sid eis full
+            percent_1 = (dist_1 / constants.MAX_DEPTH_CM) * 100
+            percent_2 = (dist_2 / constants.MAX_DEPTH_CM) * 100
+            # Averages percentages
+            avg = (percent_1 + percent_2) / 2
+            # writes level to database
+            dbc.garbageQuery(avg)
+            # If the average level is high enough, send a full alert
+            if (avg > 100):
+                dbc.alertQuery("Full")
+            elif(avg > constants.NEAR_FULL_PERCENT):
+                dbc.alertQuery("Near full")
+        # Turns sensors off
+        relay_pin.value(0)
 
-    avg = (dist_1 + dist_2) / 2
+        """ TODO: Battery level code """
 
-    dbc.garbageQuery(avg)
+        # Puts code into deepsleep
+        deepsleep(60000 * constants.DEEPSLEEP_TIME_MIN)
+
+def
