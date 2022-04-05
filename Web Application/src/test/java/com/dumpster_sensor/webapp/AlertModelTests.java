ee344a3f46c22a69397138e565ebfa6d30de8c8c
BSieck1 <brendan-sieck@uiowa.edu>
2022-04-05 14:07:18 -0500

    br

--- Web Application/src/test/java/com/dumpster_sensor/webapp/AlertModelTests.java
+++ Web Application/src/test/java/com/dumpster_sensor/webapp/AlertModelTests.java
@@ -25,7 +25,7 @@ class AlertModelTests {
     @Autowired
     private AlertRepo aRepo;
 
-    //testin
+    //testi
     @Test
     void whenValidConstructorsAndFindAll_thenNoErrors() {
         Alert a1 = new Alert(7L, "power", 1L, false);
