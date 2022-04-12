1936b45eb0ebde091b5aca89e56a84bd20cc4a37
henry-bulluck <thbulluck@gmail.com>
2022-04-11 20:29:33 -0500

    Updated php file for sensor table

--- Embedded System/database_connection.php
+++ Embedded System/database_connection.php
@@ -50,10 +50,54 @@ elseif(isset($_GET["error"]) and
 
     $connection->close();
 }
-elseif(isset($_GET["sensor_id"]) and $_GET["read_sensor"])
+elseif(isset($_GET["sensor_id"]) and
+    isset($_GET["read_sensor"]))
 {
     $read_sensor = $_GET["read_sensor"];
     $sensor_id = $_GET["sensor_id"];
+
+    // Create MySQL connection fom PHP to MySQL server
+    $connection = new mysqli($servername, $username, $password, $database_name);
+    // Check connection
+    if ($connection->connect_error) {
+        die("MySQL connection failed: " . $connection->connect_error);
+    }
+
+    $sql = "SELECT * FROM sensor WHERE id=$sensor_id";
+    $result = $connection->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            // Returns in order
+            // id, installed, power, time1, time2
+            echo $row["id"]. "," . $row["installed"]. "," . $row["power"]. "," . $row["time1"]. "," . $row["time2"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+}
+elseif(isset($_GET["sensor_id"]) and
+    isset($_GET["power"]))
+{
+    $power = $_GET["power"];
+    $sensor_id = $_GET["sensor_id"];
+
+    // Create MySQL connection fom PHP to MySQL server
+    $connection = new mysqli($servername, $username, $password, $database_name);
+    // Check connection
+    if ($connection->connect_error) {
+        die("MySQL connection failed: " . $connection->connect_error);
+    }
+
+    $sql = "UPDATE sensor SET power=$power WHERE id=$sensor_id";
+
+    if ($connection->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error: " . $sql . " => " . $connection->error;
+    }
 }
 else{
     echo "Data is not set in the HTTP request";
